331a3b25fb71b89158b2093f67d401ef757f8b2a
Beshoy <beshoy-moawad@elarabygroup.com>
2023-09-09 04:04:31 +0300

    add favs

--- stores/favorites.ts
+++ stores/favorites.ts
@@ -1,33 +1,45 @@
-// store/favorites.js
 import { defineStore } from 'pinia';
+
+// Define a type for Project to make the code more type-safe and self-documenting.
+type Project = {
+  id: string | number;
+  [key: string]: any;  // This allows the Project object to have other properties in addition to 'id'.
+};
 
 export const useFavoritesStore = defineStore('favorites', {
   state: () => ({
-    favorites: [],
+    favorites: [] as Project[],
   }),
 
   actions: {
-    // Add a project to favorites
-    addFavorite(project: any) {
-      this.favorites.push(project);
+    /**
+     * Adds a project to the favorites list.
+     * @param {Project} project - The project to be added.
+     */
+    addFavorite(project: Project) {
+      if (!this.isFavorite(project.id)) {
+        this.favorites.push(project);
+      }
     },
 
-    // Remove a project from favorites
-    removeFavorite(projectId  : any) {        
-      const index = this.favorites.findIndex((project: { id: any; }) => project.id === projectId);
+    /**
+     * Removes a project from the favorites list by its ID.
+     * @param {string | number} projectId - The ID of the project to be removed.
+     */
+    removeFavorite(projectId: string | number) {
+      const index = this.favorites.findIndex((project: Project) => project.id === projectId);
       if (index !== -1) {
         this.favorites.splice(index, 1);
       }
     },
-    isFavorite(projectId:any) {
-        return this.favorites.some((project: { id: any; }) => project.id === projectId);
-      },
-  },
 
-//   getters: {
-//     // Check if a project is a favorite
-//     isFavorite(projectId:any) {
-//       return this.favorites.some((project: { id: any; }) => project.id === projectId);
-//     },
-//   },
+    /**
+     * Checks if a project with a specific ID is already in the favorites list.
+     * @param {string | number} projectId - The ID of the project to check.
+     * @returns {boolean} - Returns true if the project is a favorite, otherwise false.
+     */
+    isFavorite(projectId: string | number): boolean {
+      return this.favorites.some((project: Project) => project.id === projectId);
+    },
+  },
 });
